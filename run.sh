c768574e6a4dde7f182bc52b7e9d3b88ae80fab4
Arthur Bernardes <arthur.bernardes1@gmail.com>
2024-09-02 16:32:20 -0300

    Delete removal_function to create another

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ detect_os() {
     case "$(uname -s)" in
         Darwin)
             # macOS
-            SED_INLINE="sed -i '' -e"
+            SED_INLINE="sed -i ''"
             ;;
         Linux)
             # Linux
-            SED_INLINE="sed -i -e"
+            SED_INLINE="sed -i"
             ;;
         *)
             echo "Sistema operacional não suportado!"
@@ -56,42 +56,6 @@ generate_vyos_routes() {
     done < tmp_prefixes.txt
 }
 
-# Funções para adicionar comandos de remoção
-add_cisco_removal() {
-    while read -r prefix; do
-        echo "no ip route $prefix Null0" >> "$output_file"
-    done < tmp_prefixes.txt
-}
-
-add_juniper_removal() {
-    echo "delete groups TWITTER-BLOCK" >> "$output_file"
-    echo "delete apply-groups TWITTER-BLOCK" >> "$output_file"
-}
-
-add_nokia_removal() {
-    while read -r prefix; do
-        echo "no configure router static-route $prefix blackhole" >> "$output_file"
-    done < tmp_prefixes.txt
-}
-
-add_huawei_removal() {
-    while read -r prefix; do
-        echo "undo ip route-static $prefix NULL0" >> "$output_file"
-    done < tmp_prefixes.txt
-}
-
-add_mikrotik_removal() {
-    while read -r prefix; do
-        echo "/ip route remove dst-address=$prefix" >> "$output_file"
-    done < tmp_prefixes.txt
-}
-
-add_vyos_removal() {
-    while read -r prefix; do
-        echo "delete protocols static route $prefix" >> "$output_file"
-    done < tmp_prefixes.txt
-}
-
 # Lista de ASNs
 asns=("63179" "54888" "35995" "13414")
 
@@ -108,10 +72,9 @@ echo "5 - Mikrotik"
 echo "6 - VyOS"
 read -p "Escolha uma opção (1-6): " choice
 
-# Define os arquivos de saída
+# Define o arquivo de saída
 output_file="static_routes.txt"
-temp_file="juniper_temp.txt"
-rm -rf "$output_file" "$temp_file"
+rm -f "$output_file"
 
 # Limpa o arquivo de saída
 > "$output_file"
@@ -147,61 +110,12 @@ for asn in "${asns[@]}"; do
             ;;
     esac
 
-    # Executa o sed para remover as linhas específicas ao ASN atual
+    # Remove as linhas específicas ao ASN atual do arquivo de saída
     $SED_INLINE "s/ip prefix-list prefix_list_${asn} permit//g" "$output_file"
-    
+
     # Remove o arquivo temporário
     rm -f tmp_prefixes.txt
 done
-
-# Adiciona os comandos de remoção ao final do arquivo
-case $choice in
-    1)
-        echo "# Commands to remove Cisco routes" >> "$output_file"
-        for asn in "${asns[@]}"; do
-            bgpq4 -4 -m 24 -l prefix_list_$asn AS$asn | grep -v '^no ip prefix-list' > tmp_prefixes.txt
-            add_cisco_removal
-            rm -f tmp_prefixes.txt
-        done
-        ;;
-    2)
-        echo "set apply-groups TWITTER-BLOCK" >> "$output_file"
-        echo "# Commands to remove Juniper routes" >> "$output_file"
-        add_juniper_removal
-        ;;
-    3)
-        echo "# Commands to remove Nokia routes" >> "$output_file"
-        for asn in "${asns[@]}"; do
-            bgpq4 -4 -m 24 -l prefix_list_$asn AS$asn | grep -v '^no ip prefix-list' > tmp_prefixes.txt
-            add_nokia_removal
-            rm -f tmp_prefixes.txt
-        done
-        ;;
-    4)
-        echo "# Commands to remove Huawei routes" >> "$output_file"
-        for asn in "${asns[@]}"; do
-            bgpq4 -4 -m 24 -l prefix_list_$asn AS$asn | grep -v '^no ip prefix-list' > tmp_prefixes.txt
-            add_huawei_removal
-            rm -f tmp_prefixes.txt
-        done
-        ;;
-    5)
-        echo "# Commands to remove Mikrotik routes" >> "$output_file"
-        for asn in "${asns[@]}"; do
-            bgpq4 -4 -m 24 -l prefix_list_$asn AS$asn | grep -v '^no ip prefix-list' > tmp_prefixes.txt
-            add_mikrotik_removal
-            rm -f tmp_prefixes.txt
-        done
-        ;;
-    6)
-        echo "# Commands to remove VyOS routes" >> "$output_file"
-        for asn in "${asns[@]}"; do
-            bgpq4 -4 -m 24 -l prefix_list_$asn AS$asn | grep -v '^no ip prefix-list' > tmp_prefixes.txt
-            add_vyos_removal
-            rm -f tmp_prefixes.txt
-        done
-        ;;
-esac
 
 echo "Rotas estáticas geradas em $output_file"
 cat "$output_file"
