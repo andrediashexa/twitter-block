aa1b881c7b480b27a5f1c4878716ba00ba68ab47
Arthur Bernardes <arthur.bernardes1@gmail.com>
2024-09-02 15:24:08 -0300

    Added removal and fixed multiple occurrences of apply groups in Juniper

--- run.sh
+++ run.sh
@@ -30,7 +30,6 @@ generate_juniper_routes() {
     while read -r prefix; do
         echo "set groups TWITTER-BLOCK routing-options static route $prefix discard" >> "$output_file"
     done < tmp_prefixes.txt
-    echo "set apply-groups TWITTER-BLOCK" >> "$output_file"
 }
 
 # Função para gerar rotas estáticas para Nokia
@@ -62,6 +61,53 @@ generate_vyos_routes() {
     done < tmp_prefixes.txt
 }
 
+# Função para adicionar comandos de remoção para Cisco
+add_cisco_removal() {
+    echo "# Commands to remove Cisco routes" >> "$output_file"
+    while read -r prefix; do
+        echo "no ip route $prefix Null0" >> "$output_file"
+    done < tmp_prefixes.txt
+}
+
+# Função para adicionar comandos de remoção para Juniper
+add_juniper_removal() {
+    echo "# Commands to remove Juniper routes" >> "$output_file"
+    echo "delete groups TWITTER-BLOCK" >> "$output_file"
+    echo "delete apply-groups TWITTER-BLOCK" >> "$output_file"
+}
+
+# Função para adicionar comandos de remoção para Nokia
+add_nokia_removal() {
+    echo "# Commands to remove Nokia routes" >> "$output_file"
+    while read -r prefix; do
+        echo "no configure router static-route $prefix blackhole" >> "$output_file"
+    done < tmp_prefixes.txt
+}
+
+# Função para adicionar comandos de remoção para Huawei
+add_huawei_removal() {
+    echo "# Commands to remove Huawei routes" >> "$output_file"
+    while read -r prefix; do
+        echo "no ip route-static $prefix NULL0" >> "$output_file"
+    done < tmp_prefixes.txt
+}
+
+# Função para adicionar comandos de remoção para Mikrotik
+add_mikrotik_removal() {
+    echo "# Commands to remove Mikrotik routes" >> "$output_file"
+    while read -r prefix; do
+        echo "/ip route remove dst-address=$prefix" >> "$output_file"
+    done < tmp_prefixes.txt
+}
+
+# Função para adicionar comandos de remoção para VyOS
+add_vyos_removal() {
+    echo "# Commands to remove VyOS routes" >> "$output_file"
+    while read -r prefix; do
+        echo "delete protocols static route $prefix" >> "$output_file"
+    done < tmp_prefixes.txt
+}
+
 # Lista de ASNs
 asns=("63179" "54888" "35995" "13414")
 
@@ -78,9 +124,10 @@ echo "5 - Mikrotik"
 echo "6 - VyOS"
 read -p "Escolha uma opção (1-6): " choice
 
-# Define o arquivo de saída
+# Define os arquivos de saída
 output_file="static_routes.txt"
-rm -rf "$output_file"
+temp_file="juniper_temp.txt"
+rm -rf "$output_file" "$temp_file"
 
 # Limpa o arquivo de saída
 > "$output_file"
@@ -93,21 +140,27 @@ for asn in "${asns[@]}"; do
     case $choice in
         1)
             generate_cisco_routes
+            add_cisco_removal
             ;;
         2)
             generate_juniper_routes
+            # Adiciona o comando apply-groups ao final do arquivo no final
             ;;
         3)
             generate_nokia_routes
+            add_nokia_removal
             ;;
         4)
             generate_huawei_routes
+            add_huawei_removal
             ;;
         5)
             generate_mikrotik_routes
+            add_mikrotik_removal
             ;;
         6)
             generate_vyos_routes
+            add_vyos_removal
             ;;
         *)
             echo "Opção inválida!"
@@ -122,6 +175,12 @@ for asn in "${asns[@]}"; do
     # Remove o arquivo temporário
     rm -f tmp_prefixes.txt
 done
+
+# Adiciona o apply-groups para Juniper no final do arquivo, se necessário
+if [ "$choice" -eq 2 ]; then
+    echo "set apply-groups TWITTER-BLOCK" >> "$output_file"
+    add_juniper_removal
+fi
 
 echo "Rotas estáticas geradas em $output_file"
 cat "$output_file"
